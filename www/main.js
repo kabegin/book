692b2236ef2e2862f2c5f326a491e806f8baeb8f
kabegin <kabegin@gmail.com>
2017-02-12 00:40:37 +0900

    Project's Initial Commit

---
+++ www/main.js
@@ -0,0 +1,30 @@
+// This is a JavaScript file
+var game=null;
+var char1=null;
+
+
+enchant();
+window.onload=function(){
+var game=new Game(window.innerWidth/2,window.innerHeight/2);
+game.preload('img/chara1.png');
+game.fps=10;
+game.onload=function(){
+    var char1=new Sprite(32,32);
+    char1.image=game.assets['img/chara1.png'];
+    char1.frame=[0,1,0,2];
+    char1.x=50;
+    char1.y=50;
+    
+    game.rootScene.backgroundColor='#eeeeff';
+    game.rootScene.addChild(char1);
+ 
+    game.rootScene.addEventListener(Event.TOUCH_START,function(event){
+     
+      char1.tl.clear();
+    　char1.tl.moveTo(event.x,event.y, 20);
+    
+    });
+};  
+
+game.start();
+};
